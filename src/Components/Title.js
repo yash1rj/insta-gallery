1e66754f5e22b6dd582ee55fb5d13f5eb366cd4e
Yash Raj <yashrj130@gmail.com>
2021-06-12 00:57:50 +0530

    Create Title.js
    Title section of phot-arcade app

---
+++ src/Components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = props => {
+    return (
+        <div className="title">
+            <h1>Photo Arcade</h1>
+            <h2>All Pictures</h2>
+            <p>Collection of curated pics.</p>
+        </div>
+    );
+};
+
+export default Title;
